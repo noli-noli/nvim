5a6327df61366f34d4b09bc05752e6eba1659601
noli-noli <gp20a051@oecu.jp>
2025-05-16 12:22:51 +0900

    vim.api.nvim_create_user_commandによるキーマップの設定

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
 require("config.lazy")
-vim.cmd [[tnoremap <silent> <ESC> <C-\><C-n>]]
-vim.cmd [[command! -nargs=* T split | wincmd j | resize 20 | terminal <args>]]
+vim.api.nvim_create_user_command("T", function(opts)
+  vim.cmd("split")
+  vim.cmd("wincmd j")
+  local total_lines = vim.o.lines
+  vim.cmd("resize " .. tostring(total_lines - 20))
+  vim.cmd("terminal " .. table.concat(opts.fargs, " "))
+end, { nargs = "*" })
 
